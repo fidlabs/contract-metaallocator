8bbadea18c76545cb526df3faded8b37dd473927
Kacper Żuk <kacper.zuk@neti-soft.com>
2024-04-08 11:25:48 +0200

    Registry -> Factory rename

--- src/interfaces/IFactory.sol
+++ src/interfaces/IFactory.sol
@@ -1,19 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.25;
 
-interface IRegistry {
-    struct Allowance {
-        address allocatorContract;
-        uint256 amount;
-    }
-
+interface IFactory {
     event Deployed(address deployedContractAddress);
 
     /// Return all allocator contracts known to the registry
     function contracts() external view returns (address[] memory);
-
-    /// Return allowances of given allocator in all contracts known to the registry
-    function allowedIn(address allocator) external view returns (Allowance[] memory);
 
     /// Deploy new allocator smart contract and register it in the registry
     /// @dev Emits Deployed event
