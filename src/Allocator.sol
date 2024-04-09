c336f399a32f6f3d925ad56fe42eeb616ad85046
Filip Lelek <filip.lelek@neti-soft.com>
2024-04-09 15:41:51 +0200

    Add more tests, contact adjustments

--- src/Allocator.sol
+++ src/Allocator.sol
@@ -5,11 +5,10 @@ pragma solidity ^0.8.25;
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {IAllocator} from "./interfaces/IAllocator.sol";
 
-contract MyContract is Initializable, OwnableUpgradeable, UUPSUpgradeable {
-    mapping(address allocatorAddress => uint256 amount) public allowances;
-
-    event AllowanceChanged(address indexed allocatorAddress, uint256 newAllowance);
+contract Allocator is Initializable, OwnableUpgradeable, UUPSUpgradeable, IAllocator {
+    mapping(address allocatorAddress => uint256 amount) public allowance;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -25,12 +24,20 @@ contract MyContract is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
     function addAllowance(address allocatorAddress, uint256 amount) external onlyOwner {
-        allowances[allocatorAddress] += amount;
-        emit AllowanceChanged(allocatorAddress, allowances[allocatorAddress]);
+        uint256 allowanceBefore = allowance[allocatorAddress];
+        allowance[allocatorAddress] += amount;
+        emit AllowanceChanged(allocatorAddress, allowanceBefore, allowance[allocatorAddress]);
     }
 
     function setAllowance(address allocatorAddress, uint256 amount) external onlyOwner {
-        allowances[allocatorAddress] == amount;
-        emit AllowanceChanged(allocatorAddress, allowances[allocatorAddress]);
+        uint256 allowanceBefore = allowance[allocatorAddress];
+        allowance[allocatorAddress] = amount;
+        emit AllowanceChanged(allocatorAddress, allowanceBefore, allowance[allocatorAddress]);
     }
+
+    // solhint-disable-next-line no-empty-blocks
+    function addVerifiedClient(address clientAddress, uint256 amount) external onlyOwner {}
+
+    // solhint-disable-next-line no-empty-blocks
+    function allocators() external view returns (address[] memory) {}
 }
