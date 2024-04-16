2bc17fef0114b9f6f8464a95ab707add2bade17d
Kacper Żuk (Neti) <117277751+kacperzuk-neti@users.noreply.github.com>
2024-04-16 13:37:58 +0200

    Update IFactory.sol

--- src/interfaces/IFactory.sol
+++ src/interfaces/IFactory.sol
@@ -5,7 +5,7 @@ interface IFactory {
     event Deployed(address deployedContractAddress);
 
     /// Return all allocator contracts known to the registry
-    function contracts() external view returns (address[] memory);
+    function getContracts() external view returns (address[] memory);
 
     /// Deploy new allocator smart contract and register it in the registry
     /// @dev Emits Deployed event
