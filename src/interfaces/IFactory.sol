6d29f9eca07e9cb9f95b6fdbe27b11ccbc8bf47b
Filip Lelek <filip.lelek@neti-soft.com>
2024-04-17 13:49:15 +0200

    Add factory contract and tests

--- src/interfaces/IFactory.sol
+++ src/interfaces/IFactory.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.25;
 
 interface IFactory {
     event Deployed(address deployedContractAddress);
+    event NewImplementationSet(address implemetation_);
 
     /// Return all allocator contracts known to the registry
     function getContracts() external view returns (address[] memory);
@@ -10,4 +11,9 @@ interface IFactory {
     /// Deploy new allocator smart contract and register it in the registry
     /// @dev Emits Deployed event
     function deploy(address owner) external;
+
+    /// Set new implementation address
+    /// @dev Emits NewImplementationSet event
+    /// @dev Reverts if not called by contract owner
+    function setImplementation(address implementation_) external;
 }
---
+++ test/Factory.t.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {Test} from "forge-std/Test.sol";
+import {Factory} from "../src/Factory.sol";
+import {Allocator} from "../src/Factory.sol";
+import {IFactory} from "../src/interfaces/IFactory.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract FactoryTest is Test {
+    Factory public factory;
+    address public impl;
+
+    function setUp() public {
+        impl = address(new Allocator());
+        factory = new Factory(address(this), impl);
+    }
+
+    function testRevertSetImplementation() public {
+        address newImpl = address(new Allocator());
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, vm.addr(1)));
+        vm.prank(vm.addr(1));
+        factory.setImplementation(newImpl);
+    }
+
+    function testSetImplementationEmitEvent() public {
+        address newImpl = address(new Allocator());
+        vm.expectEmit();
+        emit IFactory.NewImplementationSet(newImpl);
+        factory.setImplementation(newImpl);
+    }
+
+    function testImplementationAfterSetImplementation() public {
+        address newImpl = address(new Allocator());
+        factory.setImplementation(newImpl);
+        assertEq(newImpl, factory.implementation());
+    }
+
+    function testUpdateContracts() public {
+        factory.deploy(address(this));
+        address[] memory contracts = factory.getContracts();
+        assertEq(contracts.length, 1);
+        factory.deploy(address(this));
+        address[] memory contractsAfterSecondDeploy = factory.getContracts();
+        assertEq(contractsAfterSecondDeploy.length, 2);
+    }
+
+    function testDeployEmitEvent() public {
+        vm.expectEmit(false, false, false, false);
+        emit IFactory.Deployed(address(vm.addr(1)));
+        factory.deploy(address(this));
+    }
+
+    function testDeployContract() public {
+        address addr = address(new Factory(address(this), impl));
+        uint32 size;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            size := extcodesize(addr)
+        }
+        assertGt(size, 0);
+    }
+}
