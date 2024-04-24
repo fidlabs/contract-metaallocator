0d53cb48cd658ac3aa009479694365ab0b605af0
Kacper Żuk <kacper.zuk@neti-soft.com>
2024-04-24 13:32:16 +0200

    Natspec improvements

--- src/Allocator.sol
+++ src/Allocator.sol
@@ -12,9 +12,21 @@ import {CommonTypes} from "filecoin-solidity/contracts/v0.8/types/CommonTypes.so
 import {FilAddresses} from "filecoin-solidity/contracts/v0.8/utils/FilAddresses.sol";
 import {EnumerableMap} from "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 
+/**
+ * @title Allocator
+ * @notice This contract functions as a middle-man for Allocators. It's made a
+ * Verifier (a.k.a. Notary) on Filecoin chain and granted DataCap that can then
+ * be granted to Clients. Granting to clients can be done by Allocators that get
+ * allowance on the contract, assigned by contract owner.
+ * @dev Contract is upgradeable via UUPS by contract owner.
+ */
 contract Allocator is Initializable, OwnableUpgradeable, UUPSUpgradeable, IAllocator {
     using EnumerableMap for EnumerableMap.AddressToUintMap;
 
+    /**
+     * @notice Enumerable mapping from allocator addresses to their current
+     * allowance
+     */
     EnumerableMap.AddressToUintMap private _allocators;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -22,6 +34,10 @@ contract Allocator is Initializable, OwnableUpgradeable, UUPSUpgradeable, IAlloc
         _disableInitializers();
     }
 
+    /**
+     * @notice Contract initializator. Should be called during deployment.
+     * @param initialOwner Initial contract owner
+     */
     function initialize(address initialOwner) public initializer {
         __Ownable_init(initialOwner);
         __UUPSUpgradeable_init();
@@ -30,30 +46,58 @@ contract Allocator is Initializable, OwnableUpgradeable, UUPSUpgradeable, IAlloc
     // solhint-disable-next-line no-empty-blocks
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
-    function allowance(address allocator) public view returns (uint256 amount) {
-        (, amount) = _allocators.tryGet(allocator);
+    /**
+     * @notice Get allowance of an allocator
+     * @param allocator Allocator to get allowance for
+     * @return allowance_ Allocator's allowance
+     */
+    function allowance(address allocator) public view returns (uint256 allowance_) {
+        (, allowance_) = _allocators.tryGet(allocator);
     }
 
-    function addAllowance(address allocatorAddress, uint256 amount) external onlyOwner {
+    /**
+     * @notice Add allowance to Allocator
+     * @param allocator Allocator that will receive allowance
+     * @param amount Amount of allowance to add
+     * @dev Emits AllowanceChanged event
+     * @dev Reverts if not called by contract owner
+     * @dev Reverts if trying to add 0 allowance
+     */
+    function addAllowance(address allocator, uint256 amount) external onlyOwner {
         if (amount == 0) revert AmountEqualZero();
-        uint256 allowanceBefore = allowance(allocatorAddress);
-        _allocators.set(allocatorAddress, allowanceBefore + amount);
-        emit AllowanceChanged(allocatorAddress, allowanceBefore, allowance(allocatorAddress));
+        uint256 allowanceBefore = allowance(allocator);
+        _allocators.set(allocator, allowanceBefore + amount);
+        emit AllowanceChanged(allocator, allowanceBefore, allowance(allocator));
     }
 
-    function setAllowance(address allocatorAddress, uint256 amount) external onlyOwner {
-        uint256 allowanceBefore = allowance(allocatorAddress);
+    /**
+     * @notice Set allowance of an Allocator. Can be used to remove allowance.
+     * @param allocator Allocator
+     * @param amount Amount of allowance to set
+     * @dev Emits AllowanceChanged event
+     * @dev Reverts if not called by contract owner
+     * @dev Reverts if setting to 0 when allocator already has 0 allowance
+     */
+    function setAllowance(address allocator, uint256 amount) external onlyOwner {
+        uint256 allowanceBefore = allowance(allocator);
         if (allowanceBefore == 0 && amount == 0) {
             revert AlreadyZero();
         } else if (amount > 0) {
-            _allocators.set(allocatorAddress, amount);
+            _allocators.set(allocator, amount);
         } else if (allowanceBefore > 0 && amount == 0) {
-            _allocators.remove(allocatorAddress);
+            _allocators.remove(allocator);
         }
-        emit AllowanceChanged(allocatorAddress, allowanceBefore, allowance(allocatorAddress));
+        emit AllowanceChanged(allocator, allowanceBefore, allowance(allocator));
     }
 
-    /// @custom:oz-upgrades-unsafe-allow-reachable delegatecall
+    /**
+     * @notice Grant allowance to a client.
+     * @param clientAddress Filecoin address of the client
+     * @param amount Amount of datacap to grant
+     * @dev Emits DatacapAllocated event
+     * @dev Reverts with InsufficientAllowance if caller doesn't have sufficient allowance
+     * @custom:oz-upgrades-unsafe-allow-reachable delegatecall
+     */
     function addVerifiedClient(bytes calldata clientAddress, uint256 amount) external {
         if (amount == 0) revert AmountEqualZero();
         uint256 allocatorBalance = allowance(msg.sender);
@@ -71,7 +115,11 @@ contract Allocator is Initializable, OwnableUpgradeable, UUPSUpgradeable, IAlloc
         VerifRegAPI.addVerifiedClient(params);
     }
 
-    function getAllocators() external view returns (address[] memory) {
+    /**
+     * @notice Get all allocators with non-zero allowance
+     * @return allocators List of allocators with non-zero allowance
+     */
+    function getAllocators() external view returns (address[] memory allocators) {
         return _allocators.keys();
     }
 }
--- src/Factory.sol
+++ src/Factory.sol
@@ -7,24 +7,51 @@ import {Allocator} from "./Allocator.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IFactory} from "./interfaces/IFactory.sol";
 
+/**
+ * @title Allocator Factory
+ * @notice Factory for deploying new instances of Allocator Contract. Factory
+ * owner can update the implementation used for deploying new instances.
+ */
 contract Factory is Ownable, IFactory {
+    /**
+     * @notice List of contracts deployed by this Factory
+     */
     address[] public contracts;
+
+    /**
+     * @notice Implementation used when deploying new contracts
+     */
     address public implementation;
 
     constructor(address initialOwner, address implementation_) Ownable(initialOwner) {
         implementation = implementation_;
     }
 
-    function getContracts() external view returns (address[] memory) {
+    /**
+     * @notice Return all Allocator contracts deployed by this Factory
+     * @return contracts_ Array of Allocator contracts
+     */
+    function getContracts() external view returns (address[] memory contracts_) {
         return contracts;
     }
 
+    /**
+     * @notice Deploy new allocator smart contract and register it in the registry
+     * @param owner Initial Owner of the deployed contract
+     * @dev Emits Deployed event
+     */
     function deploy(address owner) external {
         address proxy = address(new ERC1967Proxy(implementation, abi.encodeCall(Allocator.initialize, (owner))));
         emit Deployed(proxy);
         contracts.push(proxy);
     }
 
+    /**
+     * @notice Set new implementation address used for deploying new contracts
+     * @param implementation_ New implementation address
+     * @dev Emits NewImplementationSet event
+     * @dev Reverts if not called by contract owner
+     */
     function setImplementation(address implementation_) external onlyOwner {
         implementation = implementation_;
         emit NewImplementationSet(implementation_);
