b8dcb6e56a5af0b1ee549aeb0d5f84b9a45f7d4e
Filip-L <67787091+Filip-L@users.noreply.github.com>
2024-05-20 12:59:40 +0200

    Set pragma to a fixed value
    Co-authored-by: Filip Lelek <filip.lelek@neti-soft.com>

--- src/Allocator.sol
+++ src/Allocator.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.25;
+pragma solidity 0.8.25;
 
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
--- src/Factory.sol
+++ src/Factory.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.25;
+pragma solidity 0.8.25;
 
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {Allocator} from "./Allocator.sol";
