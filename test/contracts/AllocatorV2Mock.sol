535b4d094ea7da2e45b0d3b4267675aeeac67638
Kacper Żuk <kacper.zuk@neti-soft.com>
2024-08-29 11:53:19 +0200

    Fix tests

---
+++ test/contracts/AllocatorV2Mock.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {Allocator} from "../../src/Allocator.sol";
+
+// solhint-disable no-empty-blocks
+/// @custom:oz-upgrades-from src/Allocator.sol:Allocator
+contract AllocatorV2Mock is Allocator {}
