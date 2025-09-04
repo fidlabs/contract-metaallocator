8dd75674834ccb5047d93cd9858ec604b275990f
Kacper Żuk <kacper.zuk@neti-soft.com>
2025-09-04 15:01:42 +0200

    Fix formatting, fix coverage checks

--- ci/check-full-coverage.sh
+++ ci/check-full-coverage.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 cd "$(dirname "$0")"/..
 
-forge clean && forge build && forge coverage --no-match-coverage "(script|test|AllocatorV1|ClientV1)" --report lcov
+forge clean && forge build && forge coverage --no-match-coverage "(script|test|Allocator|Client)" --report lcov
 
 summary=$(lcov --summary lcov.info --rc branch_coverage=1)
 
@@ -14,9 +14,10 @@ branches_coverage=$(echo "$summary" | awk '/branches/{print $2}')
 echo "Lines coverage: $lines_coverage"
 echo "Functions coverage: $functions_coverage"
 echo "Branches coverage: $branches_coverage"
-if [ "$lines_coverage" == "100.0%" ] && [ "$functions_coverage" == "100.0%" ] && [ "$branches_coverage" == "100.0%" ] ; then
-    echo "Coverage is 100% for lines, functions and branches."
+if [ "$lines_coverage" == "100.0%" ] && [ "$functions_coverage" == "100.0%" ] && [ "$branches_coverage" == "100.0%" ]; then
+  echo "Coverage is 100% for lines, functions and branches."
 else
-    echo "Coverage is not 100%."
-    exit 1
+  echo "Coverage is not 100%."
+  exit 1
 fi
+
